e78e0f3d0e003f2f4515cf55da1d0c7d7407ccfc
physnerds <amitbashyal@gmail.com>
2021-07-20 12:34:43 -0700

    memcpy for the non char to char copy

--- h5demos/highfive/serialize_dataprods.h
+++ h5demos/highfive/serialize_dataprods.h
@@ -19,7 +19,7 @@ std::vector<std::string> return_dsetnames(TObjArray* l) {
     TClass* class_ptr=nullptr;
     EDataType type;
     b->GetExpectedType(class_ptr,type);
-    //  if(class_ptr==nullptr)continue; //need to uncomment it if we want to skip the null pointers....
+    //   if(class_ptr==nullptr)continue; //need to uncomment it if we want to skip the null pointers....
     names.push_back(b->GetName());
   }
   return names;
@@ -36,7 +36,7 @@ std::vector<TClass*> return_classes(TObjArray* l) {
     TClass* class_ptr=nullptr;
     EDataType type;
     b->GetExpectedType(class_ptr,type);
-    // if(class_ptr==nullptr)continue;
+    //if(class_ptr==nullptr)continue;
     classes.push_back( class_ptr);
   }
   return classes;
@@ -51,21 +51,30 @@ std::vector<char> return_blob(TBranch* b, TClass* c) {
 }
 
 
+
+
 std::vector<char>return_fundamental_blobs(TBranch *b){
   TBufferFile bufferFile{TBuffer::kWrite};
   auto leaves = b->GetListOfLeaves();
   //Right now for the testing purpose, I will just assume there will be one leaf in the fundamental type branches....
   auto leaf = dynamic_cast<TLeaf*>((*leaves)[0]);
   auto c_type = leaf->GetTypeName();
-  std::stringstream _type;
-  _type<<c_type;
   std::vector<char>blob;
   auto _length = leaf->GetLen();
-  for(int i = 0;i<_length;i++){
-    blob.push_back(char(leaf->GetValue(i)));
+  if(_length!=0){
+    for(int i = 0;i<_length;i++){
+      auto _val = leaf->GetValue(i);
+      char arr_val[sizeof(_val)];
+      memcpy(arr_val,&_val,sizeof(_val));
+      blob.insert(blob.begin(),std::begin(arr_val),std::end(arr_val));
+      // blob.push_back(arr_val);
 
+    }
   }
-  
+  else{
+    blob.push_back('\0');
+    
+  }
   /*
   if(_type.str()=="Int_t"){
     auto _length = leaf->GetLen();
